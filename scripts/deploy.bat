47b4212e267c04dfbf0ff86a5d4c3c0a97a59a1c
Marc A. Donis <marc@balloonary.io>
2024-05-01 22:16:47 +0200

    .

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -3,11 +3,11 @@ call npm run build
 del /s /q ..\vue-github-deploy-dist\*
 xcopy dist ..\vue-github-deploy-dist /s /e
 git checkout deployment
-del /s /q js\*
-del /s /q css\*
-xcopy ..\vue-github-deploy-dist . /s /e /y
-git add .
-git commit -a
-git push
-git checkout master
-cd scripts
+@REM del /s /q js\*
+@REM del /s /q css\*
+@REM xcopy ..\vue-github-deploy-dist . /s /e /y
+@REM git add .
+@REM git commit -a
+@REM git push
+@REM git checkout master
+@REM cd scripts
