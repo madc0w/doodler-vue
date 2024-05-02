f1c94392021a1dc738d8c805adbbc57f04de779a
Marc A. Donis <marc@balloonary.io>
2024-05-02 07:48:22 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === 'production' ? '/<project-name>/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/doodler-vue/' : '/',
 });
